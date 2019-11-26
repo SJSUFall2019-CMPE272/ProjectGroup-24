5af28cc3febcab4617560503ea0d7942ade1db8d
mudit777 <uditniraj.marolia@sjsu.edu>
2019-11-25 16:58:38 -0800

    profile UI

--- Frontend/src/components/Profile/Profile.js
+++ Frontend/src/components/Profile/Profile.js
@@ -3,6 +3,7 @@ import {
   Jumbotron,
   Button,
   Col,
+  Row,
   Form,
   FormGroup,
   Label,
@@ -12,6 +13,7 @@ import axios from "axios";
 import { hostAddress, port } from "../../config";
 import Modal from "react-modal";
 import { ButtonGroup, Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const config = {
   headers:{
@@ -22,7 +24,7 @@ const config = {
 
 const customStyles = {
   content: {
-    top: "50%",
+    top: "55%",
     left: "50%",
     right: "auto",
     bottom: "auto",
@@ -36,18 +38,44 @@ class Profile extends Component {
     super();
     this.state = {
       modalIsOpen: false,
-      email:"",
-      message:""
+      fullname:"Cristiano Ronaldo",
+      email:"admin@admin.com",
+      address:"383 Stockton Avenue",
+      zipcode:"95126",
+      city:"San Jose",
+      country:"USA",
+      mobile:"8866215479",
+      updateFlag:false
     };
 
     this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
-    this.inputChangeHandler = this.inputChangeHandler.bind(this);
-    this.sendMessage = this.sendMessage.bind(this);
+    // this.inputChangeHandler = this.inputChangeHandler.bind(this);
+    // this.sendMessage = this.sendMessage.bind(this);
   }
   //get the books data from backend
-  componentDidMount() {}
+  componentDidMount() {
+ axios.defaults.withCredentials = true;
+// const data={email: "admin@admin.com"}
+//         //make a post request with the user data
+        axios.post('http://'+hostAddress+':'+port+'/profile/profile',data,config)
+                .then((response) => {
+                    
+                   
+                this.setState({
+                email : response.data.email,
+                fullname: response.data.fullname,
+                mobile : response.data.mobile,
+                address : response.data.address,
+                city : response.data.city,
+                zipcode : response.data.zipcode,
+                country : response.data.country
+                //image : "http://"+hostAddress+":"+port+"/images/all/" + response.data.image+ "" 
+                });
+                
+            });
+  }
 
   inputChangeHandler = e => {
     console.log("Blehh")
@@ -69,11 +97,151 @@ class Profile extends Component {
     this.setState({ modalIsOpen: false });
   }
 
-  sendMessage = e => {
-   
+  updateProfile = e => {
+   this.setState({
+     updateFlag:true
+   })
   }
 
   render() {
+
+let display=null;
+if(!this.state.updateFlag){
+console.log("holaa")
+display=(
+  <div style ={{margin:"0 10% 0 10%"}}>
+    <div className="row"  style ={{fontSize:"30px",justifyContent:"center"}}>{this.state.fullname}</div>
+    <div className="row" style ={{fontSize:"15px"}}>
+      <div className="col-md-6"><label>Email:</label></div>
+      <div className="col-md-6"><label>Mobile:</label></div>
+    </div>
+<div className= "row" style ={{fontSize:"20px",justifyContent:"center"}}>
+  <div className= "col-md-6">{this.state.email}</div>
+  <div className= "col-md-6">{this.state.mobile}</div>
+</div>
+<div className="row" style ={{fontSize:"15px"}}>
+      <div className="col-md-6"><label>Address:</label></div>
+      <div className="col-md-6"><label>ZipCode:</label></div>
+</div>
+<div className="row" style={{fontSize:"20px"}}>
+  <div className="col-md-6">{this.state.address}</div>
+  <div className="col-md-6">{this.state.zipcode}</div>
+</div>
+
+<div className="row" style ={{fontSize:"15px"}}>
+      <div className="col-md-6"><label>City:</label></div>
+      <div className="col-md-6"><label>Country:</label></div>
+</div>
+<div className="row" style={{fontSize:"20px"}}>
+  <div className="col-md-6">{this.state.city}</div>
+  <div className="col-md-6">{this.state.country}</div>
+</div>
+<div>
+<ButtonGroup style={{justifyContent:"center"}}>
+                  <Button
+                    color="info"
+                    style={{
+                      fontSize: "15px",
+                      margin: "0 5% 0 0",
+                      width: "80px"
+                    }}
+                    onClick={this.updateProfile}
+                  >
+                    Update
+                  </Button>
+                  <Button
+                    color="danger"
+                    style={{
+                      fontSize: "15px",
+                      margin: "0 5% 0 0",
+                      width: "80px"
+                    }}
+                    onClick={this.closeModal}
+                  >
+                    Cancel
+                  </Button>
+                </ButtonGroup>
+</div>
+</div>
+)
+}else{
+  display=(
+   
+      <Form>
+      <Row form>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="Email" style={{fontSize:"15px"}}>Email</Label>
+            <Input type="email" name="email" id="Email" style={{fontSize:"20px"}} placeholder={this.state.email} />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="Mobile" style={{fontSize:"15px"}}>Mobile</Label>
+            <Input type="text" name="password" id="Mobile" style={{fontSize:"20px"}} placeholder={this.state.mobile} />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row form>
+        <Col md={6}>
+          <FormGroup>
+          <Label for="Address" style={{fontSize:"15px"}}>Address</Label>
+           <Input type="text" name="address" id="exampleAddress" style={{fontSize:"20px"}} placeholder={this.state.address}/>
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+          <Label for="Zip" style={{fontSize:"15px"}}>Zip Code</Label>
+            <Input type="text" name="zip" id="Zip" style={{fontSize:"20px"}} placeholder={this.state.zipcode}/>
+          </FormGroup>
+        </Col>
+      </Row>
+      
+      <Row form>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="City" style={{fontSize:"15px"}}>City</Label>
+            <Input type="text" name="city" id="City" style={{fontSize:"20px"}} placeholder={this.state.city}/>
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="Country" style={{fontSize:"15px"}}>Country</Label>
+            <Input type="text" name="country" id="Country" style={{fontSize:"20px"}} placeholder={this.state.country}/>
+          </FormGroup>
+        </Col>
+      </Row>
+     
+     
+      <ButtonGroup style={{justifyContent:"center"}}>
+                  <Button
+                    color="info"
+                    style={{
+                      fontSize: "15px",
+                      margin: "0 5% 0 0",
+                      width: "80px"
+                    }}
+                    onClick={this.updateProfile}
+                  >
+                    Update
+                  </Button>
+                  <Button
+                    color="danger"
+                    style={{
+                      fontSize: "15px",
+                      margin: "0 5% 0 0",
+                      width: "80px"
+                    }}
+                    onClick={this.closeModal}
+                  >
+                    Cancel
+                  </Button>
+                </ButtonGroup>
+  </Form>
+  )
+}
+
+
     return (
       <Jumbotron>
         <div class="container">
@@ -98,38 +266,18 @@ class Profile extends Component {
               style={customStyles}
               contentLabel="Example Modal"
             >
-              <h2 ref={subtitle => (this.subtitle = subtitle)}>Contact Us</h2>
+              <h2 ref={subtitle => (this.subtitle = subtitle)}>Profile</h2>
 
-              <Card style={{ width: '30rem' }}>
-  <Card.Img variant="top" style={{width:"80px", height:"100px", margin:"0 35% 0 35%", borderRadius:"50px"}} src="https://images.unsplash.com/photo-1454942901704-3c44c11b2ad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" />
+              <Card style={{ width: '50rem' }}>
+  <Card.Img variant="top " style={{width:"80px", height:"80px", margin:"0 40% 0 40%", borderRadius:"100px"}} src="https://images.unsplash.com/photo-1454942901704-3c44c11b2ad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
     
-
-                <ButtonGroup style={{ float: "right", marginRight: "5px" }}>
-                  <Button
-                    color="info"
-                    style={{
-                      fontSize: "90%",
-                      margin: "0 5% 0 0",
-                      width: "80px"
-                    }}
-                    onClick={this.sendMessage}
-                  >
-                    Update
-                  </Button>
-                  <Button
-                    color="danger"
-                    style={{ fontSize: "90%", margin: "0 5% 0 0" }}
-                    onClick={this.closeModal}
-                  >
-                    Cancel
-                  </Button>
-                </ButtonGroup>
+ 
+    
+    {display}
+                
+                
+               
                 </Card.Body>
 </Card>
             </Modal>
