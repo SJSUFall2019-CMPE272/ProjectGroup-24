dffa88f07c4246329a567114422ec527dff68b0b
Alaukika Diwanji <alaukikadiwanji@gmail.com>
2019-11-27 12:21:46 -0800

    Added Search Table

--- Frontend/src/components/Search/Search.js
+++ Frontend/src/components/Search/Search.js
@@ -5,7 +5,7 @@ import Modal from "react-modal";
 import { ButtonGroup, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Redirect } from "react-router";
-
+import './Search.css';
 const config = {
   headers: {
     Authorization: "Bearer " + localStorage.getItem("jwtToken"),
@@ -19,9 +19,10 @@ class Search extends Component {
     super();
     this.state = {
      searchText:"",
-     regions:[]
+     regions:[],
+     searchFlag:false,
+     searchResult:[{"disease":"Duseaese","drug":["drug 1, drug 2"]},{"disease":"Duseaese","drug":["drug 1, drug 2"]},{"disease":"Duseaese", "drug":["drug 1, drug 2"]}]
     };
-
    
     this.inputChangeHandler = this.inputChangeHandler.bind(this);
   
@@ -41,6 +42,11 @@ class Search extends Component {
       });
     }
   
+    searchRegion=e=>{
+      this.setState({
+        searchFlag:true
+      });
+    }
 
   inputChangeHandler = e => {
     console.log("Blehh");
@@ -52,38 +58,73 @@ class Search extends Component {
   render() {
     let display = null;
     let redirec = null;
+    let tablecontent=[];
     if (localStorage.getItem("email") == null) {
       redirec = <Redirect to="/home" />;
     }
+if(!this.state.searchFlag){
+  display=(  
+  <div>
+    <input
+      type="text"
+      name="searchText"
+      id="Search"
+      size="30"
+      align="middle"
+      style={{
+        height: "50px",
+        margin: "15% 20% 30% 32%",
+        fontSize: "35px",
+        borderRadius: "25px"
+      }}
+    ></input>
+    <button onClick={this.searchRegion.bind(this)}>PressMe</button>
+  </div>
+)
+}else{
+  let details=this.state.searchResult.map(item=>{
+    tablecontent.push(<tr><td  style={{}}>{item.disease}</td><td  style={{}}>{item.drug}</td></tr>
+    )
+
+    })
+
+display=(
+<div class="container">
+  <div style={{textAlign:"center"}}>
+    <span style={{justifyContent:"space-evenly", marginTop:"100px",fontSize:"18px", color:"white"}}>Location : </span>
+    <span>{this.state.searchText}</span>
+    </div>
+    <table class="table" style={{backgroundColor:"white", margin:"30px"}}>    
+    <thead>
+      <th>Disease</th>
+      <th>Drug</th>
+      </thead>                    
+                        <tbody>
+                          
+                            {/*Display the Tbale row based on data recieved*/}
+                            {tablecontent}
+                        </tbody>
+                    </table>
+
+</div>)
 
   
+}
+  
     return (
-      <div>
+      <div style={{
+        backgroundImage:
+          "url(https://www.freewalldownload.com/cristiano-ronaldo/portugal-free-cristiano-ronaldo-hd-football-mobile-desktop-download-wallpapers-images.jpg)",
+        backgroundSize: "100% 100%"
+      }}>
         {redirec}
         <div
-          style={{
-            backgroundImage:
-              "url(https://www.freewalldownload.com/cristiano-ronaldo/portugal-free-cristiano-ronaldo-hd-football-mobile-desktop-download-wallpapers-images.jpg)",
-            backgroundSize: "100% 100%"
-          }}
         >
-          <h3 class="heading element-animate">
-            <div>
-              <input
-                type="text"
-                name="searchText"
-                id="Search"
-                size="30"
-                align="middle"
-                style={{
-                  height: "50px",
-                  margin: "15% 20% 30% 32%",
-                  fontSize: "35px",
-                  borderRadius: "25px"
-                }}
-              ></input>
-            </div>
-          </h3>
+           <div>
+           {display}
+     
+           </div>
+     
           <br></br>
 
          
