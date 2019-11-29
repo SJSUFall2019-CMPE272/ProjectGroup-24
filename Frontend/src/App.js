f5de45ad8c54bd51eda9d4ada87a805cbefcaac8
Alaukika Diwanji <alaukikadiwanji@gmail.com>
2019-11-28 18:18:13 -0800

    new change hostname 2

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './components/Main';
 import {BrowserRouter} from 'react-router-dom';
+require('dotenv').config()
 
 //App Component
 class App extends Component {
